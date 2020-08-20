559f67739ce9615e174ede506f32b02e703fee92
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-20 17:06:05 +0300

    refactoring

--- bash/update.sh
+++ bash/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+cd /root/rd1 && \
 git pull && \
 docker system prune -f && \
-docker build -t paveldemidyuk/rd1-app:arm .
+docker build -t paveldemidyuk/rd1-app:arm . && \
+docker push paveldemidyuk/rd1-app:arm
 
