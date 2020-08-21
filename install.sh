25277f61ccec92148413509cc402d61b5b745f51
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-21 23:21:54 +0300

    refactoring

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ export PATH
 # step 1: login to dietpi and setup internet connection
 # step 2: run this file
 # install docker
-/boot/dietpi/dietpi-software install 162 || exit 1
+/boot/dietpi/dietpi-software install 162 || exit 1 &
 # install avahi
-/boot/dietpi/dietpi-software install 152 || exit 1
+/boot/dietpi/dietpi-software install 152 || exit 1 &
 # install chromium
-/boot/dietpi/dietpi-software install 113 || exit 1
+/boot/dietpi/dietpi-software install 113 || exit 1 &
 
 
 # pull rd1-app
-docker pull eu.gcr.io/rd1-build/rd1-app:arm
+docker pull eu.gcr.io/rd1-build/rd1-app:arm &
 # pull rd1-owserver
-docker pull eu.gcr.io/rd1-build/rd1-owserver:arm
+docker pull eu.gcr.io/rd1-build/rd1-owserver:arm &
 # pull mqtt
-docker pull eclipse-mosquitto:latest
+docker pull eclipse-mosquitto:latest &
 #setup rd1 service
-
+wait
 
 #setup hassio
 sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential
