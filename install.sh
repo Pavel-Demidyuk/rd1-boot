203d21e75ed14f544825e46d08319f7f0b000297
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-21 23:07:35 +0300

    refactoring

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# step 1: login to dietpi and setup internet connection
+# step 2: run this file
+
+# install docker
+dietpi-software install 162
+# install avahi
+dietpi-software install 152
+# install chromium
+dietpi-software install 113
+
+
+# pull rd1-app
+docker pull eu.gcr.io/rd1-build/rd1-app:arm
+# pull rd1-owserver
+docker pull eu.gcr.io/rd1-build/rd1-owserver:arm
+# pull mqtt
+docker pull eclipse-mosquitto:latest
+
+#setup rd1 service
+
+
+#setup hassio
+sudo apt-get install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential
+sudo useradd -rm homeassistant -G dialout,gpio,i2c
+cd /srv
+sudo mkdir homeassistant
+sudo chown homeassistant:homeassistant homeassistant
+sudo -u homeassistant -H -s
+cd /srv/homeassistant
+python3 -m venv .
+source bin/activate
+python3 -m pip install wheel
+pip3 install homeassistant
+
+cp services/home-assistant@homeassistant.service /etc/systemd/system/home-assistant@homeassistant.service
+sudo systemctl --system daemon-reload
+sudo systemctl enable home-assistant@homeassistant
+
+# install display driver
+#chmod -R 755 LCD-show
+#bash LCD-show/LCD35-show
+
