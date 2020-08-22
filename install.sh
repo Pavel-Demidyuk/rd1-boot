49d7c2eac45aec6a5e6462a87bfe5d97394996d5
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-22 17:25:37 +0300

    refactoring

--- install.sh
+++ install.sh
@@ -52,6 +52,6 @@
 
 # install display driver
 chmod -R 755 /root/rd1-boot/LCD-show || exit 1
-bash cd /root/rd1-boot/LCD-show/ && ./LCD35-show || exit 1
+cd /root/rd1-boot/LCD-show/ && ./LCD35-show || exit 1
 
 reboot
