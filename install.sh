fd68673d29617c9b0be3baa05c506b6a9cbce6e3
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-22 16:24:56 +0300

    refactoring

--- install.sh
+++ install.sh
@@ -30,13 +30,8 @@ sudo useradd -rm homeassistant -G dialout,gpio,i2c
 cd /srv
 sudo mkdir homeassistant
 sudo chown homeassistant:homeassistant homeassistant
-sudo -u homeassistant -H -s
-cd /srv/homeassistant
-python3 -m venv .
-source bin/activate
-python3 -m pip install wheel
-pip3 install homeassistant
-
+sudo -u homeassistant -H -s && cd /srv/homeassistant && python3 -m venv . && source bin/activate && python3 -m pip install wheel && pip3 install homeassistant
+sudo su
 cp services/home-assistant@homeassistant.service /etc/systemd/system/
 sudo systemctl --system daemon-reload
 sudo systemctl enable home-assistant@homeassistant
