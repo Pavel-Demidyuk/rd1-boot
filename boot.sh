ce956ba8d93efc359a45603c20a48365475af192
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-23 11:03:13 +0300

    refactoring

--- boot.sh
+++ boot.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# INITIAL RD1 Configurator
+
+# this file should be called remotely by next command:
+#  bash -c "$(curl -sSL https://raw.githubusercontent.com/Pavel-Demidyuk/rd1-boot/master/boot.sh)"
+
+
 wget --no-check-certificate https://github.com/Pavel-Demidyuk/rd1-boot/archive/master.zip
 unzip master.zip
 mv rd1-boot-master rd1-boot
