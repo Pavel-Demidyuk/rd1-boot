9ecc3c2b77b2c25ef71a9ce0a857c5e5d00b186d
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-23 11:22:23 +0300

    refactoring

--- boot.sh
+++ boot.sh
@@ -11,4 +11,4 @@
 wget --no-check-certificate https://github.com/Pavel-Demidyuk/rd1-boot/archive/master.zip
 unzip master.zip
 mv rd1-boot-master rd1-boot
-cd rd1-boot && run install.sh
+cd rd1-boot && install.sh
