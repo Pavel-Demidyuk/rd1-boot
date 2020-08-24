b5e180db5f6ab45bba12b815d80110767ad0dcd2
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-24 19:44:12 +0300

    refactoring

---
+++ install_hassio.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt-get -y install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential
+sudo useradd -rm homeassistant -G dialout,gpio,i2c
+cd /srv
+sudo mkdir homeassistant
+sudo chown homeassistant:homeassistant homeassistant
+su homeassistant <<'EOF'
+cd /srv/homeassistant
+python3 -m venv .
+source bin/activate
+python3 -m pip install wheel
+pip3 install homeassistant
+EOF
---
+++ install_rpi.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# step 1: login to dietpi and setup internet connection
+# step 2: bash -c "$(wget --no-check-certificate https://cutt.ly/rd1-start && cat rd1-start)"
+
+#TODO enable i2c
+
+sudo apt-get update && sudo apt-get upgrade || exit 1
+curl -fsSL https://get.docker.com -o get-docker.sh || exit 1
+sudo sh get-docker.sh || exit 1
+
+# install chromium
+sudo apt-get install chromium-browser --yes
+
+#install fbi
+sudo apt-get install -y fbi
+
+# pull rd1-app
+docker pull eu.gcr.io/rd1-build/rd1-app:arm || exit 1  &
+# pull rd1-dashboard
+docker pull eu.gcr.io/rd1-build/rd1-dashboard:arm || exit 1  &
+# pull rd1-owserver
+docker pull eu.gcr.io/rd1-build/rd1-owserver:arm || exit 1 &
+# pull mqtt
+docker pull eclipse-mosquitto:latest || exit 1 &
+# pull nodered
+docker pull nodered/node-red:latest || exit 1 &
+wait
+
+##setup hassio
+bash hassio.sh || exit 1
+
+#enable hassio service
+cp /root/rd1-boot/services/home-assistant@homeassistant.service /etc/systemd/system/
+sudo systemctl --system daemon-reload
+sudo systemctl enable home-assistant@homeassistant
+
+# register rd1 service
+cp /root/rd1-boot/services/rd1.service /etc/systemd/system/
+sudo systemctl --system daemon-reload
+sudo systemctl enable rd1
+
+# install display driver
+chmod -R 755 /root/rd1-boot/LCD-show || exit 1
+cd /root/rd1-boot/LCD-show/ && ./LCD35-show || exit 1
+
+#reboot
+#
