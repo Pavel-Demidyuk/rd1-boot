a62d359489c11b6d5126d37b6433e362bdd3bf02
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-25 17:37:47 +0300

    refactoring

--- install_hassio.sh
+++ install_hassio.sh
@@ -12,7 +12,7 @@ source bin/activate
 python3 -m pip install wheel
 pip3 install homeassistant
 EOF
-sudo su
+sudo pi
 cd /home/pi/rd1-boot
 #enable hassio service
 cp /home/pi/rd1-boot/services/home-assistant@homeassistant.service /etc/systemd/system/
