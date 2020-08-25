992bf5c4e985b4f8a11f46d7b118873068a99ce5
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-25 13:31:29 +0300

    refactoring

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@
 
 #TODO enable i2c
 
-printf "\n\n\n ---- Update RPI ---- \n"
+echo -e "\e[42m\n\n\n ---- Update RPI ---- \n\e[39mDefault"
 sudo apt-get update --yes && sudo apt-get upgrade  --yes || exit 1
 
-printf "\n\n\n ---- Install DOCKER ---- \n"
+echo -e "\e[42m\n\n\n ---- Install DOCKER ---- \n\e[39mDefault"
 docker -v || (curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh)
 
-printf "\n\n\n ---- Install Chromium ---- \n"
+echo -e "\e[42m\n\n\n ---- Install Chromium ---- \n\e[39mDefault"
 # install chromium
 chromium-browser --version || sudo apt-get install chromium-browser --yes
 
-printf "\n\n\n ---- Install FBI ---- \n"
+echo -e "\e[42m\n\n\n ---- Install FBI ---- \n\e[39mDefault"
 #install fbi
 fbi --version || sudo apt-get install -y fbi
 
-printf "\n\n\n ---- Pull DOCKER ---- \n"
+echo -e "\e[42m\n\n\n ---- Pull DOCKER ---- \n\e[39mDefault"
 # pull rd1-app
 docker pull eu.gcr.io/rd1-build/rd1-app:arm || exit 1  &
 # pull rd1-dashboard
@@ -31,21 +31,21 @@ docker pull eclipse-mosquitto:latest || exit 1 &
 docker pull nodered/node-red:latest || exit 1 &
 wait
 
-printf "\n\n\n ---- Install HASSIO ---- \n"
+echo -e "\e[42m\n\n\n ---- Install HASSIO ---- \n\e[39mDefault"
 ##setup hassio
 bash install_hassio.sh || exit 1
 
-printf "\n\n\n ---- Register rd1 service ---- \n"
+echo -e "\e[42m\n\n\n ---- Register rd1 service ---- \n\e[39mDefault"
 # register rd1 service
 cp /home/pi/rd1-boot/services/rd1.service /etc/systemd/system/
 sudo systemctl --system daemon-reload
 sudo systemctl enable rd1
 
-printf "\n\n\n ---- Install Display Drivers ---- \n"
+echo -e "\e[42m\n\n\n ---- Install Display Drivers ---- \n\e[39mDefault"
 # install display driver
 chmod -R 755 /home/pi/rd1-boot/Lroot/rd1-bootCD-show || exit 1
 cd /home/pi/rd1-boot/LCD-show/ && ./LCD35-show || exit 1
 
-printf "\n\n\n ---- REBOOT!! ---- \n"
+echo -e "\e[42m\n\n\n ---- REBOOT!! ---- \n\e[39mDefault"
 sleep 3
 reboot
