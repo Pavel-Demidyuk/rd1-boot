e330b7f2c0f0e22e65da745cdb12825aa3e718af
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-26 23:59:00 +0300

    refactoring

--- install.sh
+++ install.sh
@@ -2,10 +2,30 @@
 # step 1: login to dietpi and setup internet connection
 # step 2: bash -c "$(wget --no-check-certificate https://cutt.ly/rd1-start && cat rd1-start)"
 
-#TODO enable i2c
+
+
+#sudo su
+#RD1_STRING="#rd1 updates"
+#grep -q $RD1_STRING /boot/config.txt || ( \
+#echo $RD1_STRING >> /boot/config.txt && \
+#echo "dtparam=i2s=on" >> /boot/config.txt && \
+#echo "dtparam=spi=on" >> /boot/config.txt && \
+#echo "dtoverlay=piscreen,speed=16000000,rotate=90" >> /boot/config.txt && \
+#echo "dtparam=i2c_arm=on" >> /boot/config.txt)
+#su pi
+
+
 
 echo -e "\e[42m\n\n\n ---- Update RPI ---- \e[0m"
 sudo apt-get update --yes && sudo apt-get upgrade  --yes || exit 1
+
+echo -e "\e[42m\n\n\n ---- Install display driver ---- \e[0m"
+./LCD-show/LCD35-show
+
+echo -e "\e[35m\n\n\n ---- Desktop setup ---- \e[0m"
+#sudo apt-get install lightdm || exit 1
+#sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+
 
 echo -e "\e[42m\n\n\n ---- Install DOCKER ---- \n\e[0m"
 docker -v || (curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh)
