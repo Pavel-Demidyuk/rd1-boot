05ac240b95d98b5c24520b2d915e298b771f8183
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-27 18:49:42 +0300

    refactoring

--- bash/services/mqtt.sh
+++ bash/services/mqtt.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 name='mqtt'
-sudo docker stop $name && echo $name 'service stopped'
-sudo docker rm $name && echo $name 'service removed'
+image='eu.gcr.io/rd1-build/eclipse-mosquitto:latest'
+sudo docker stop $image && echo $name 'service stopped'
+sudo docker rm $image && echo $name 'service removed'
 sudo docker run -d --name=$name \
 	--network=host \
 	--restart=always \
@@ -9,4 +10,4 @@ sudo docker run -d --name=$name \
 	--health-timeout=10s \
 	--health-retries=1 \
 	--health-interval=10m \
-	eclipse-mosquitto:latest && echo $name 'service started'
+	$image && echo $name 'service started'
--- bash/services/nodered.sh
+++ bash/services/nodered.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 name='node-red'
-sudo docker stop $name && echo $name 'service stopped'
-sudo docker rm $name && echo $name 'service removed'
+image='eu.gcr.io/rd1-build/node-red:latest'
+sudo docker stop $image && echo $name 'service stopped'
+sudo docker rm $image && echo $name 'service removed'
 sudo docker run -d \
     --name=$name \
 	--network=host \
 	--restart=always \
 	-v  /root/rd1/boot/nodered/data:/data \
-	nodered/node-red:latest && echo $name 'service started'
+	$image && echo $name 'service started'
