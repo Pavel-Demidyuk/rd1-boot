e5963097e0e253b904f5c0daa5ec629e7e7a0bb2
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-28 16:37:01 +0300

    refactoring

---
+++ autostart.sh
@@ -0,0 +1,6 @@
+#/home/pi/.config/lxsession/LXDE-pi/autostart
+
+#chromium-browser --kiosk --no-first-run --check-for-update-interval=864000000 --noerrdialogs --disable-infobars --test-type --window-size=480,320 --start-fu$
+#sleep 180
+#chromium-browser --kiosk --no-first-run --check-for-update-interval=864000000 --noerrdialogs --disable-infobars --test-type --window-size=480,320 --start-fu$
+
