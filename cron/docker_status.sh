76650aa927104c2db867572244b473efd30f18f8
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-30 23:17:17 +0300

    refactoring

--- cron/docker_status.sh
+++ cron/docker_status.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 FILE='/tmp/rd1-status-docker.json'
+COMMAND="sudo docker container ls -a --format '{{.Names}}***{{.Status}}'"
 truncate -s 0 $FILE
-DATA=$(sudo docker container ls -a --format '{{.Names}}***{{.Status}}')
+DATA=$($COMMAND)
 echo $DATA >> $FILE
