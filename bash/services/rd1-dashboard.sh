3a6ce2bf76f3c42aca0f255167cefc18e4cc5c4b
pdemidyuk <pdemidyuk@infoblox.com>
2020-08-30 23:34:27 +0300

    refactoring

--- bash/services/rd1-dashboard.sh
+++ bash/services/rd1-dashboard.sh
@@ -5,8 +5,8 @@ sudo docker stop $name && echo $name 'service stopped'
 sudo docker kill $name && echo $name 'service killed'
 sudo docker rm $name && echo $name 'service removed'
 sudo docker run -d --name=$name \
+	-p 3000:3000 \
 	--restart=on-failure \
-	--priveleged \
 	-v /proc/sysrq-trigger:/sysrq \
     --health-cmd='rm -rf index.html && wget --spider -q http://localhost:3000 && exit 0 || exit 1' \
 	--health-timeout=10s \
